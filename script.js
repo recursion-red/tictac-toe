e4db62b2e480cfc7f8e65fe1ac16f41a46c974b0
Tomoya1 <tomoya.tmy@gmail.com>
2023-02-28 08:54:14 +0900

    勝利判定と画面の表示を実装

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
+const config = {
+    mainPage: document.getElementById("mainPage"),
+    winJudgeMessage: document.getElementById("winJudgeMessage"),
+}
+
+// 非表示にする
+function displayNone(ele){
+    ele.classList.remove("d-block");
+    ele.classList.add("d-none");
+}
+
+// 非表示を表示させる
+function displayBlock(ele){
+    ele.classList.remove("d-none");
+    ele.classList.add("d-block");
+}
+
 // buttonとuserTurnを取得
 const button = document.querySelectorAll(".button");
 const userTurn = document.getElementById("user-turn");
+// 勝ちパターンを定義
+const winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 // 現在のTurnを変数に代入
 let turnCount = 1;
+let count = 1;
 
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", function () {
     // buttonの中身が空だったら、情報を更新するupdateInfo関数を呼び出す
     if (button[i].innerHTML == "") {
-      updateInfo(button[i]);
+        updateInfo(button[i]);
+
+        // draw判定
+        if(count >= 9) {
+        renderResult("draw")
+        }
+
+        // 勝利判定
+        winerCheck()
+        count++
     }
   });
 }
@@ -27,6 +65,24 @@ function updateInfo(button) {
   // 現在のturnを更新
   turnCount++;
 }
+
+// 勝利判定の関数
+function winerCheck() {
+    for(let i in winPattern) {
+        let element1 = button[winPattern[i][0]].innerHTML;
+        let element2 = button[winPattern[i][1]].innerHTML;
+        let element3 = button[winPattern[i][2]].innerHTML;
+
+        // 勝利判定
+        if(element1 != "" && element2 != "" && element3 != "") {
+            if(element1 == element2 && element2 == element3) {
+                // winの画面を表示させる関数を呼び出す
+                renderResult(element1);
+            }
+        }
+    }
+}
+
 
 
 // リザルト画面を表示する
@@ -61,19 +117,21 @@ function renderResult(winPlayer){
     */
     modal.classList.remove('d-block');
     modal.classList.add('d-none');
+    // countを初期化する
+    count = 1;
   })
 }
 
-/* 確認用 
-start ===*/
-const btnCheckResult = document.querySelector('.checkResult');
-const winPlayer = 'X';
+// /* 確認用 
+// start ===*/
+// const btnCheckResult = document.querySelector('.checkResult');
+// const winPlayer = 'X';
 
-function main(){
- btnCheckResult.addEventListener('click', () => {
-  renderResult(winPlayer);
- }) 
-}
+// function main(){
+//  btnCheckResult.addEventListener('click', () => {
+//   renderResult(winPlayer);
+//  }) 
+// }
 
-main();
-/* ==== end */
+// main();
+// /* ==== end */
