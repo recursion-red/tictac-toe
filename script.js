a76bc520c213af1672cdca630ad70f84b0f04a63
Admin of Recursion! <fnifhubi85h29bddi@gmail.com>
2023-03-04 11:38:02 +0900

    クリック制御

--- script.js
+++ script.js
@@ -16,11 +16,16 @@ const winPattern = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
+// selectModeを取得
+const selectMode = document.getElementById("select-mode");
+// あらかじめ現在のvalueを代入
+let currentGameMode = selectMode.value;
 // 埋まっているindexを保存
 let fillIndex = [];
 // ボタンを押した回数
 let count = 1;
+// cpu動作をするかしないか 0だったらする、1だったらしない
+let cpuControl = 0;
 
 // 非表示にする
 function displayNone(ele) {
@@ -32,6 +37,10 @@ function displayNone(ele) {
 function displayBlock(ele) {
   ele.classList.remove("d-none");
   ele.classList.add("d-block");
+}
+
+function turnAnimation() {
+  console.log("テスト");
 }
 
 // 勝利判定の関数
@@ -53,6 +62,9 @@ function winerCheck() {
 
 // リザルト画面を表示する
 function renderResult(winPlayer) {
+  // リザルト画面になってからのcpuの動作を制御
+  cpuControl = 1;
+
   let btnReset = document.getElementById("modal-btn-reset");
   let modalWinner = document.getElementById("modal-winner");
 
@@ -66,8 +78,7 @@ function renderResult(winPlayer) {
   displayBlock(config.modal);
 
   // リセットボタン処理
-  clickResetBtn(btnReset)
-
+  clickResetBtn(btnReset);
 }
 
 function clickResetBtn(btnReset) {
@@ -80,6 +91,9 @@ function clickResetBtn(btnReset) {
     // modalを非表示、テーブルを表示
     displayNone(config.modal);
     displayBlock(config.mainPage);
+
+    // なぜか無駄な数字が出てくる
+    // console.log(count);
 
     // userのturnを初期化
     userTurn.innerHTML = "X";
@@ -107,10 +121,13 @@ function getCpuIndex(index) {
 
 function hoverButton(button) {
   button.addEventListener("mouseover", function () {
-      if (button.getAttribute("check-now") == null) {
-        button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
-        button.classList.add("text-secondary");
-      }
+    if (
+      button.getAttribute("check-now") == null &&
+      (currentGameMode == "player" || userTurn.innerHTML == "X")
+    ) {
+      button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
+      button.classList.add("text-secondary");
+    }
   });
 
   button.addEventListener("mouseout", function () {
@@ -120,17 +137,16 @@ function hoverButton(button) {
   });
 }
 
-function addTextOX (button) {
+function addTextOX(button) {
+  button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
+  userTurn.innerHTML = userTurn.innerHTML == "X" ? "O" : "X";
+  button.classList.remove("text-secondary");
+  button.setAttribute("check-now", "1");
 
-      button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
-      userTurn.innerHTML = userTurn.innerHTML == "X" ? "O" : "X";
-      button.classList.remove("text-secondary");
-      button.setAttribute("check-now", "1");
+  // 勝利判定
+  winerCheck();
 
-      // 勝利判定
-      winerCheck();
-
-      count++;
+  count++;
 }
 
 // ボタン処理
@@ -140,25 +156,69 @@ for (let i = 0; i < button.length; i++) {
 
   // クリック処理
   button[i].addEventListener("click", function () {
-    if (button[i].getAttribute("check-now") == null) {
-      // draw判定
+    if (
+      currentGameMode == "cpu" &&
+      userTurn.innerHTML == "X" &&
+      button[i].getAttribute("check-now") == null
+    ) {
+      cpuControl = 0;
+
       if (count >= 9) {
         renderResult("draw");
       }
 
       // クリックされた箇所にO or Xを追加
-      addTextOX(button[i])
+      addTextOX(button[i]);
 
       // CPU処理
       const cpuIndex = getCpuIndex(i);
-      if (cpuIndex != "end") {
-        setTimeout(()=>{
-          addTextOX(button[cpuIndex])
-        }, 800);
+      if (cpuControl == 0 && cpuIndex != "end") {
+        turnAnimation();
+        setTimeout(() => {
+          addTextOX(button[cpuIndex]);
+          turnAnimation();
+        }, 1500);
       }
+    } else if (
+      currentGameMode == "player" &&
+      button[i].getAttribute("check-now") == null
+    ) {
+      turnAnimation();
 
+      // countでdraw判定をしているので
+      // draw判定
+      if (count >= 9) {
+        renderResult("draw");
+      } else {
+        addTextOX(button[i]);
+      }
     }
   });
 }
 
+// ゲームモード反映
+selectMode.addEventListener("change", function () {
+  const message = confirm(
+    "ゲームモードを切り替えますか？(現在の進行状況はリセットされます)"
+  );
 
+  if (message) {
+    // 現在のモードを更新
+    currentGameMode = selectMode.value;
+
+    // ゲームをリセット
+    for (let i = 0; i < button.length; i++) {
+      button[i].removeAttribute("check-now");
+      button[i].innerHTML = "";
+    }
+
+    // userのturnを初期化
+    userTurn.innerHTML = "X";
+    // fillIndexを初期化
+    fillIndex = [];
+    // countを初期化する
+    count = 1;
+  } else {
+    selectMode.value = currentGameMode;
+  }
+});
