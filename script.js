551830344c0a99e3fc98eacff9559d3f0c4c796f
goemon-github <booya.8108@gmail.com>
2023-03-04 21:39:33 +0900

    temp

--- script.js
+++ script.js
@@ -25,6 +25,35 @@ let fillIndex = [];
 // ボタンを押した回数
 let count = 1;
 
+
+let players = {
+  p1: 'x',
+  p2: 'o'
+}
+
+// cpuの状態
+let cpuStatus = {
+  player: 'O',
+  state: false,
+  level: 'easy',
+}
+
+function initGame() {
+  if (currentGameMode == ('eazy' || 'medium' || 'hard')) {
+    cpuStatus.state = true;
+    cpuStatus.level = currentGameMode;
+  } else {
+    cpuStatus.state = false;
+  }
+
+  // userのturnを初期化
+  userTurn.innerHTML = "X";
+  // fillIndexを初期化
+  fillIndex = [];
+  // countを初期化する
+  count = 1;
+}
+
 // 非表示にする
 function displayNone(ele) {
   ele.classList.remove("d-block");
@@ -49,9 +78,11 @@ function winerCheck() {
       if (element1 == element2 && element2 == element3) {
         // winの画面を表示させる関数を呼び出す
         renderResult(element1);
+        return true;
       }
     }
   }
+  return false;
 }
 
 // リザルト画面を表示する
@@ -85,25 +116,19 @@ function clickResetBtn() {
   displayNone(config.modal);
   displayBlock(config.mainPage);
 
-  // userのturnを初期化
-  userTurn.innerHTML = "X";
-  // fillIndexを初期化
-  fillIndex = [];
-  // countを初期化する
-  count = 1;
+  initGame();
   
 }
 
 // CPUを作成する関数
 function getCpuIndex(index) {
-  if (fillIndex.length == 8) return "end";
+  if (fillIndex.length == 8) return -1;
   fillIndex.push(index);
   let cpuIndex = 0;
 
   while (fillIndex.indexOf(cpuIndex) != -1) {
     cpuIndex = Math.floor(Math.random() * 9);
   }
-
   fillIndex.push(cpuIndex);
 
   return cpuIndex;
@@ -112,7 +137,7 @@ function getCpuIndex(index) {
 function hoverButton(button) {
   button.addEventListener("mouseover", function () {
     if (button.getAttribute("check-now") == null) {
-      if (currentGameMode == "player") {
+      if (currentGameMode == "pvp") {
         button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
         button.classList.add("text-secondary");
       }
@@ -139,15 +164,18 @@ function addTextOX(button) {
   button.setAttribute("check-now", "1");
   button.style.pointerEvents = 'none';
 
+  /*
   // 勝利判定
   winerCheck();
 
   count++;
+  */
 
 }
 
 function addTextCPU(button) {
-  return new Promise((res, _) {
+  return new Promise(function (res, _) {
+    setTimeout(() => {
       button.innerHTML = userTurn.innerHTML == "X" ? "&#10005" : "&#9675";
       userTurn.innerHTML = userTurn.innerHTML == "X" ? "O" : "X";
       button.classList.remove("text-secondary");
@@ -158,39 +186,46 @@ function addTextCPU(button) {
       winerCheck();
 
       count++;
-  })
+      res();
+    }, 700);
+  });
+}
+
+async function writeOX(button, i){
+  if (currentGameMode === 'pvp' || cpuStatus.player != userTurn.innerHTML) {
+    addTextOX(button[i]);
+    let winCheck = winerCheck();
+    count++;
+
+    console.log('winCheckFlag',winCheck);
+    if (cpuStatus.state && (cpuStatus.player == userTurn.innerHTML) && !winCheck)  {
+      let cpuIndex = await getCpuIndex(i);
+      await addTextCPU(button[cpuIndex]);
+    }
+  }
 
 }
 
-// ボタン処理
-for (let i = 0; i < button.length; i++) {
-  // ボタンのhover処理
-  hoverButton(button[i]);
 
-  // クリック処理
-  button[i].addEventListener('click', function() { 
-    if (button[i].getAttribute("check-now") == null) {
-        // draw判定
-        if (count >= 9) {
-          renderResult("draw");
-        }
-        if (currentGameMode === 'player' || count % 2 != 0) {
-          // クリックされた箇所にO or Xを追加
-          console.log('p X');
-          addTextOX(button[i]);
-        }
+function gameStart ()  {
 
-        // CPU処理
-        const cpuIndex = getCpuIndex(i);
-        if (currentGameMode == "cpu" && cpuIndex != "end") {
-          setTimeout(() => {
-            addTextOX(button[cpuIndex]);
-            console.log('p O');
-          }, 500);
-        }
+  // ボタン処理
+  for (let i = 0; i < button.length; i++) {
 
-      }
-  });
+    // ボタンのhover処理
+    hoverButton(button[i]);
+
+    // クリック処理
+    button[i].addEventListener('click', function() { 
+      if (button[i].getAttribute("check-now") == null) {
+          // draw判定
+          if (count >= 9) {
+            renderResult("draw");
+          }
+          writeOX(button, i);
+        }
+    },);
+  }
 }
 
 // ゲームモード反映
@@ -202,9 +237,65 @@ selectBox.addEventListener("change", function () {
   if (message) {
     // 現在のモードを更新
     currentGameMode = selectBox.value;
+    if (currentGameMode === ('eazy', 'medium', 'hard')) {
+      cpuStatus.state = true;
+      cpuStatus.level = currentGameMode;
+    }
     // ゲームをリセット
     clickResetBtn();
   } else {
     selectBox.value = currentGameMode;
   }
 });
+
+// MinMax 
+let board = [
+  '', '', '',
+  '', '', '',
+  '', '', '',
+]
+
+function emptySearch(board) {
+  let spaces = [];
+  for (let i = 0; i < board.length; i++){
+    if (board[i] == '') spaces.push(i);
+  }
+  return spaces;
+}
+
+function isWin(board, player) {
+  for (let i in winPattern) {
+    let element1 = board[winPattern[i][0]].innerHTML;
+    let element2 = board[winPattern[i][1]].innerHTML;
+    let element3 = board[winPattern[i][2]].innerHTML;
+
+    // 勝利判定
+    if (element1 != "" && element2 != "" && element3 != "") {
+      if (element1 == element2 && element2 == element3) {
+        // winの画面を表示させる関数を呼び出す
+        renderResult(element1);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function miniMax(board, player){
+  // 空のインデックスを見つける
+  let emptyIndex = emptySearch(board);
+
+  // ポイントを格納する
+  let result = [];
+
+}
+
+/*
+  miniMax(board, players.p1);
+  miniMax(board, players.p2);
+*/
+
+/*
+initGame();
+gameStart();
+*/
